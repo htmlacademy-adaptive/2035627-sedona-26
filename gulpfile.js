fe98a3ab1f64e181cc806d00d04ba4a5e5e7963b
Marharyta Overednaya <pandoraaa274@gmail.com>
2022-09-13 15:20:28 +0300

    Производительность

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ import svgo from "gulp-svgmin";
 import svgstore from "gulp-svgstore";
 import del from "del";
 import browser from "browser-sync";
+import htmlmin from "gulp-htmlmin";
 
 // Styles
 
@@ -19,7 +20,7 @@ export const styles = () => {
     .src("source/less/style.less", { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([autoprefixer(), csso()]))
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
@@ -27,8 +28,8 @@ export const styles = () => {
 
 // HTML
 
-const html = () => {
-  return gulp.src("source/*.html").pipe(gulp.dest("build"));
+ export const html = () => {
+  return gulp.src("source/*.html").pipe(htmlmin({collapseWhitespace: true})).pipe(gulp.dest("build"));
 };
 
 // Scripts
